26098ba923bb79a2914446d6f81b1b69e5ab1aa3
masha4321 <masharik09@mail.ru>
2022-05-29 20:58:13 -0400

    MENU CAN BE DISPLAYED

--- src/main/java/net/javaguides/springboot/web/MainController.java
+++ src/main/java/net/javaguides/springboot/web/MainController.java
@@ -1,7 +1,10 @@
 package net.javaguides.springboot.web;
 
+import net.javaguides.springboot.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -14,5 +17,15 @@ public class MainController {
 	@GetMapping("/")
 	public String home() {
 		return "index";
+	}
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@GetMapping({"/menu"})
+	public ModelAndView getAllItems() {
+		ModelAndView mav = new ModelAndView("menu");
+		mav.addObject("items", itemRepository.findAll());
+		return mav;
 	}
 }
