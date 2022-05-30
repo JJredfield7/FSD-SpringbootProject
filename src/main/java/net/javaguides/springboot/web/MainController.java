3636b38fa1ab34edb7aeb2d3c5e02b256604808e
JJredfield7 <jjredfield7@gmail.com>
2022-05-29 20:29:55 -0700

    Maryia's version

--- src/main/java/net/javaguides/springboot/web/MainController.java
+++ src/main/java/net/javaguides/springboot/web/MainController.java
@@ -1,6 +1,7 @@
 package net.javaguides.springboot.web;
 
 import net.javaguides.springboot.repository.ItemRepository;
+import net.javaguides.springboot.repository.RestaurantInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,16 @@ public class MainController {
 	public ModelAndView getAllItems() {
 		ModelAndView mav = new ModelAndView("menu");
 		mav.addObject("items", itemRepository.findAll());
+		return mav;
+	}
+
+	@Autowired
+	private RestaurantInfoRepository restoRepository;
+
+	@GetMapping({"/contactUs"})
+	public ModelAndView getAllRestaurants() {
+		ModelAndView mav = new ModelAndView("contactUs");
+		mav.addObject("restaurants", restoRepository.findAll());
 		return mav;
 	}
 }
